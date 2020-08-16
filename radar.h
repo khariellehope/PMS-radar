afc89d434cd0c328ec1b548a68568159d442ef80
Kharielle Hernandez <kharielle.h.hernandez@student.uts.edu.au>
2020-08-16 23:12:56 +1000

    update header

--- radar.h
+++ radar.h
@@ -5,12 +5,19 @@ class Radar
 {
 public:
     Radar();
-    void setScanningTime(double scanning_time);
-    void getSensorData;
+    void setScanningTime(unsigned int);
+    int getScanningTime(void);
+    unsigned int getNumberofTargets(void);
+    void changeScanningTime(void);
+    void printParameters(void);
+    void getData(void);
+    
+    
 
 private:
     unsigned int scanning_time_;
     unsigned int max_distance_;
+    int getRandomNumber(double, double);
 };
 
 #endif // RADAR_H
